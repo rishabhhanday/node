08f1f90401c2fb6c65ca2cf6be9419a06864ad81
Rishabh Handay <rishabhhanday.rh@gmail.com>
2020-07-22 10:12:57 +0530

    heroku port configured

--- src/index.js
+++ src/index.js
@@ -25,8 +25,8 @@ app.get('/intro', (req, res) => {
     });
 });
 
+const port = process.env.PORT || 3000;
 
-
-app.listen('3000', () => {
+app.listen(port, () => {
     console.log('application is running');
 });
